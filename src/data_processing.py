3a0080f627d4a07d39cb0d0f6c0512aa70a94cde
Alok <69.uralok@gmail.com>
2025-05-14 14:28:18 +0530

    Implemented the data processing

---
+++ src/data_processing.py
@@ -0,0 +1,30 @@
+# src/data_processing.py
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def load_data(data_path):
+    df = pd.read_csv(data_path)
+    return df
+
+def preprocess_data(df):
+    df = pd.get_dummies(df, columns=['location'], prefix='location', drop_first=True) #for hot encoding
+    # Separate features (X) and target (y)
+    X = df.drop('price', axis=1)
+    y = df['price']
+    return X, y
+
+def split_data(X, y, test_size=0.2, random_state=42):
+    """Splits the features and target into training and testing sets."""
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    return X_train, X_test, y_train, y_test
+
+if __name__ == '__main__':
+    data_path = "C:\\Users\\Alok singh\\projects\\housePricePrediction\\data\\house_price.csv"
+    df = load_data(data_path)
+    if df is not None:
+        X, y = preprocess_data(df)
+        X_train, X_test, y_train, y_test = split_data(X, y)
+        print("Shape of X_train:", X_train.shape)
+        print("Shape of X_test:", X_test.shape)
+        print("Shape of y_train:", y_train.shape)
+        print("Shape of y_test:", y_test.shape)
